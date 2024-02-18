dc2ec304ede6eb3226c59a5710eb4c0c8d667adb
leejh7 <777joonho@kookmin.ac.kr>
2024-02-18 18:42:47 +0900

    Project Init

---
+++ src/test/java/org/capstone/maru/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capstone.maru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
