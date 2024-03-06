c479e775e2fb2dac2b42474ed816a9f0b4692222
leejh7 <777joonho@kookmin.ac.kr>
2024-03-07 03:13:57 +0900

    feat #4 - 로그인 시 멤버에 필요한 비지니스 로직 작성, dto 생성, 테스트 로그인 시 기존에 회원이 등록 되어 있으면 회원을 찾아야하므로 searchMember 로직 구현 회원 가입 시 회원 정보로 회원을 등록해야하므로 saveUser 로직 구현 위 로직에 따른 테스트 코드 작성
    서비스 레이어의 메서드 안에서는 레포지토리 레이어와 상호작용하며 반환 값은 컨트롤러 레이어에서 사용할 것이므로 XXXDto로 반환

--- src/main/java/org/capstone/maru/service/MemberAccountService.java
+++ src/main/java/org/capstone/maru/service/MemberAccountService.java
@@ -1,2 +1,40 @@
-package org.capstone.maru.service;public class MemberAccountService {
+package org.capstone.maru.service;
+
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.capstone.maru.domain.MemberAccount;
+import org.capstone.maru.dto.MemberAccountDto;
+import org.capstone.maru.repository.MemberAccountRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class MemberAccountService {
+
+    private final MemberAccountRepository memberAccountRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<MemberAccountDto> searchMember(String memberId) {
+        return memberAccountRepository.findById(memberId)
+                                      .map(MemberAccountDto::from);
+    }
+
+    public MemberAccountDto saveUser(
+        String memberId,
+        String email,
+        String nickname
+    ) {
+        return MemberAccountDto.from(
+            memberAccountRepository.save(
+                MemberAccount.of(
+                    memberId,
+                    email,
+                    nickname,
+                    memberId
+                )
+            )
+        );
+    }
 }
