b4d81dabed7db74b6f76e7204ca8e7ab3224af14
leejh7 <777joonho@kookmin.ac.kr>
2024-03-07 04:55:34 +0900

    feat #2 - 스프링 시큐리티 및 OAuth2.0 코드 단 설정 현재 개발단계에서 h2 console을 사용하기 위해 해당 url들을 열어놓음 기본적인 카카오 oauth2.0 설정과 시큐리티 설정
    추후에 기획에 맞게 커스터마이즈 필요

--- src/main/java/org/capstone/maru/controller/MainController.java
+++ src/main/java/org/capstone/maru/controller/MainController.java
@@ -1,2 +1,20 @@
-package org.capstone.maru.controller;public class MainController {
+package org.capstone.maru.controller;
+
+import org.capstone.maru.dto.security.SharedPostPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "home";
+    }
+
+    @GetMapping("/test")
+    public String test(@AuthenticationPrincipal SharedPostPrincipal sharedPostPrincipal) {
+        return sharedPostPrincipal.getName();
+    }
 }
