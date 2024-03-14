47c83c3c34309c178ef80fc513c0fa0756c5f4ca
leejh7 <777joonho@kookmin.ac.kr>
2024-03-15 02:40:46 +0900

    feat #13 - 에러 코드와 응답 메시지 형식 지정
    - spring에 동일한 이름을 가진 클래스가 존재하여 클래스 이름 변경
    - RestErrorResponse record 타입으로 변경
    - 인증되지 않은 사용자 에러 코드 추가
    - 존재하지 않는 리소스에 접근 에러 코드 추가

--- src/main/java/org/capstone/maru/exception/RestErrorCode.java
+++ src/main/java/org/capstone/maru/exception/RestErrorCode.java
@@ -3,12 +3,11 @@ package org.capstone.maru.exception;
 import lombok.Getter;
 
 @Getter
-public enum ErrorCode {
+public enum RestErrorCode {
 
-    /**
-     * 이미 있는 값
-     */
     DUPLICATE_VALUE(409, "C001", "이미 존재하는 값입니다."),
+    UNAUTHORIZED(401, "C001", "인증 되지 않은 사용자입니다."),
+    NOT_FOUND(404, "C001", "존재하지 않는 url 입니다."),
     ;
 
     // 에러 코드의 '코드 상태'을 반환한다.
@@ -20,7 +19,7 @@ public enum ErrorCode {
     // 에러 코드의 '코드 메시지'을 반환한다.
     private final String message;
 
-    ErrorCode(final int status, final String code, final String message) {
+    RestErrorCode(final int status, final String code, final String message) {
         this.status = status;
         this.code = code;
         this.message = message;
