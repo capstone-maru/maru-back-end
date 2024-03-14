375e43b0d3986130cc51426073abbfd870740868
leejh7 <777joonho@kookmin.ac.kr>
2024-03-15 02:37:13 +0900

    feat #13 - Login 컨트롤러 구현
    카카오, 네이버 로그인 url path 구현

--- src/main/java/org/capstone/maru/controller/MainController.java
+++ src/main/java/org/capstone/maru/controller/MainController.java
@@ -1,12 +1,12 @@
 package org.capstone.maru.controller;
 
+
 import lombok.RequiredArgsConstructor;
 import org.capstone.maru.security.principal.SharedPostPrincipal;
 import org.capstone.maru.service.MemberAccountService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,6 +24,4 @@ public class MainController {
 
         return sharedPostPrincipal.getName();
     }
-
-
 }
