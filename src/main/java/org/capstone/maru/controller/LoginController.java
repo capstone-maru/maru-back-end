e86cdbae9c362f5f4c4a6d0d6bd70af7a85f873b
leejh7 <777joonho@kookmin.ac.kr>
2024-03-16 23:44:19 +0900

    refactor #17 - controller 수정
    LoginController는 필요가 없다고 판단하여 현재는 비움.
    추후에 프론트와 맞추면서 로그인 페이지에서 필요한 정보가 있다면 그때 추가할 수 있게 파일은 지우지 않음.

--- src/main/java/org/capstone/maru/controller/LoginController.java
+++ src/main/java/org/capstone/maru/controller/LoginController.java
@@ -10,18 +10,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class LoginController {
 
-    @GetMapping("/login")
-    public String socialLogin() {
-        return "카카오 로그인 url: login-kakao | 네이버 로그인 url: login-naver";
-    }
-
-    @GetMapping("/login-kakao")
-    public void loginKakao(HttpServletResponse response) throws IOException {
-        response.sendRedirect("oauth2/authorization/kakao");
-    }
-
-    @GetMapping(value = "/login-naver")
-    public void loginNaver(HttpServletResponse response) throws IOException {
-        response.sendRedirect("oauth2/authorization/naver");
-    }
 }
