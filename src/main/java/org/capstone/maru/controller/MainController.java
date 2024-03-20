db07ce4d6b643d4524013df06acf2f41c5cf89e9
leejh7 <777joonho@kookmin.ac.kr>
2024-03-20 11:20:55 +0900

    feat #9 - 에러 코드 추가, 테스트를 위한 url

--- src/main/java/org/capstone/maru/controller/MainController.java
+++ src/main/java/org/capstone/maru/controller/MainController.java
@@ -1,15 +1,23 @@
 package org.capstone.maru.controller;
 
 
+import java.util.Collections;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.capstone.maru.security.principal.SharedPostPrincipal;
+import org.capstone.maru.security.token.TokenProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class MainController {
+
+    private final TokenProvider tokenProvider;
 
     @GetMapping("/")
     public String root() {
@@ -20,5 +28,32 @@ public class MainController {
     public String test(@AuthenticationPrincipal SharedPostPrincipal sharedPostPrincipal) {
 
         return sharedPostPrincipal.getName();
+    }
+
+    @GetMapping("/token-test")
+    public String tokenTest() {
+
+        Authentication auth = new OAuth2AuthenticationToken(
+            SharedPostPrincipal.of("kakao_3377745316", "777joonho@naver.com", "이준호",
+                Map.of("id", "3377745316",
+                    "connected_at", "2024-03-06T19:18:04Z",
+                    "properties", Map.of("nickname", "이준호"),
+                    "kakao_account", Map.of("profile_nickname_needs_agreement", false,
+                        "profile", Map.of("nickname", "이준호", "is_default_nickname", false),
+                        "name_needs_agreement", false,
+                        "name", "이준호",
+                        "has_email", true,
+                        "email_needs_agreement", false,
+                        "is_email_valid", false,
+                        "is_email_verified", true,
+                        "email", "777joonho@naver.com"
+                    )
+                )
+            ),
+            Collections.singleton(new SimpleGrantedAuthority("ROLE_MEMBER")),
+            "kakao"
+        );
+        String accessToken = tokenProvider.createAccessToken(auth);
+        return accessToken;
     }
 }
--- src/main/java/org/capstone/maru/exception/RestErrorCode.java
+++ src/main/java/org/capstone/maru/exception/RestErrorCode.java
@@ -5,9 +5,10 @@ import lombok.Getter;
 @Getter
 public enum RestErrorCode {
 
-    DUPLICATE_VALUE(409, "C001", "이미 존재하는 값입니다."),
     UNAUTHORIZED(401, "C001", "인증 되지 않은 사용자입니다."),
+    INVALID_TOKEN_VALUE(401, "C002", "유효하지 않은 토큰입니다."),
     NOT_FOUND(404, "C001", "존재하지 않는 url 입니다."),
+    DUPLICATE_VALUE(409, "C001", "이미 존재하는 값입니다."),
     ;
 
     // 에러 코드의 '코드 상태'을 반환한다.
