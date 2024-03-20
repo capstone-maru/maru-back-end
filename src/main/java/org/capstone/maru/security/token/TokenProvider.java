24d53cbd95abc45e84b8a2e70c7f5b773389b477
leejh7 <777joonho@kookmin.ac.kr>
2024-03-20 11:09:23 +0900

    feat #9 - Token 기반 인증, 인가를 위한 인터페이스 구현
    Jwt가 아닌 다른 토큰 기반 인증, 인가 방식을 고려하여 인터페이스로 Token 관련 기능을 뽑음

---
+++ src/main/java/org/capstone/maru/security/token/TokenProvider.java
@@ -0,0 +1,21 @@
+package org.capstone.maru.security.token;
+
+import io.jsonwebtoken.JwtException;
+import org.capstone.maru.security.principal.SharedPostPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+
+public interface TokenProvider {
+
+    boolean validate(String token);
+
+    OAuth2AuthenticationToken decode(String token);
+
+    TokenDto reissueAccessTokenUsing(String refreshToken) throws JwtException;
+
+    String createAccessToken(Authentication authentication);
+
+    String createRefreshToken(Authentication authentication);
+
+    Long getExpiration();
+}
---
+++ src/main/java/org/capstone/maru/security/token/TokenReIssuer.java
@@ -0,0 +1,9 @@
+package org.capstone.maru.security.token;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.capstone.maru.security.exception.InvalidTokenException;
+
+public interface TokenReIssuer {
+
+    TokenDto reissueAccessToken(HttpServletRequest request) throws InvalidTokenException;
+}
