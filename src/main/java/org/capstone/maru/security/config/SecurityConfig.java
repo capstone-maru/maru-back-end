4a381bc87825b1a9ea5f2184b74bc546ba85806e
leejh7 <777joonho@kookmin.ac.kr>
2024-03-20 11:22:47 +0900

    feat #9 - config 파일에 토큰 기능 적용하도록 추가
    session 기반이 아닌 토큰 기반이므로 SessionManagement에서 session을 stateless로 설정함
    토큰을 생성하는 로직이 있는 authSuccessHandler 추가
    토큰을 검증하는 로직이 있는 tokenAuthenticationProcessingFilter 추가

--- src/main/java/org/capstone/maru/security/config/SecurityConfig.java
+++ src/main/java/org/capstone/maru/security/config/SecurityConfig.java
@@ -2,33 +2,47 @@ package org.capstone.maru.security.config;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.capstone.maru.security.filter.TokenAuthenticationProcessingFilter;
 import org.capstone.maru.security.service.CustomOAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
+@Order(0)
 @Configuration
 public class SecurityConfig {
 
     private final AuthenticationEntryPoint authEntryPoint;
-
     private final AuthenticationFailureHandler authFailureHandler;
+    private final AuthenticationSuccessHandler authSuccessHandler;
+    private final TokenAuthenticationProcessingFilter tokenAuthenticationProcessingFilter;
 
     public SecurityConfig(
         @Qualifier("customAuthenticationEntryPoint") AuthenticationEntryPoint authEntryPoint,
-        @Qualifier("customAuthenticationFailureHandler") AuthenticationFailureHandler authFailureHandler
+        @Qualifier("customAuthenticationFailureHandler") AuthenticationFailureHandler authFailureHandler,
+        @Qualifier("customAuthenticationSuccessHandler") AuthenticationSuccessHandler authenticationSuccessHandler,
+        @Autowired TokenAuthenticationProcessingFilter tokenAuthenticationProcessingFilter
     ) {
         this.authEntryPoint = authEntryPoint;
         this.authFailureHandler = authFailureHandler;
+        this.authSuccessHandler = authenticationSuccessHandler;
+        this.tokenAuthenticationProcessingFilter = tokenAuthenticationProcessingFilter;
     }
 
     @Bean
@@ -48,7 +62,7 @@ public class SecurityConfig {
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .requestMatchers(
                     HttpMethod.GET,
-                    "/"
+                    "/", "/token-test"
                 ).permitAll()
                 .requestMatchers(
                     HttpMethod.POST,
@@ -56,11 +70,15 @@ public class SecurityConfig {
                 ).permitAll()
                 .anyRequest().authenticated()
             )
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
             .oauth2Login(oAuth -> oAuth
                 .userInfoEndpoint(userInfo -> userInfo
                     .userService(customOAuth2UserService)
                 )
                 .failureHandler(authFailureHandler)
+                .successHandler(authSuccessHandler)
             )
             .exceptionHandling(hc -> hc
                 .authenticationEntryPoint(authEntryPoint)
@@ -73,6 +91,10 @@ public class SecurityConfig {
                 csrf -> csrf
                     .ignoringRequestMatchers("/api/**")
                     .disable()
+            )
+            .addFilterBefore(
+                tokenAuthenticationProcessingFilter,
+                UsernamePasswordAuthenticationFilter.class
             )
             .build();
     }
