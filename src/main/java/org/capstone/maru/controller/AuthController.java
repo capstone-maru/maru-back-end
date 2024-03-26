cc0766f98b0a6ec79b356754237cae15908279eb
leejh7 <777joonho@kookmin.ac.kr>
2024-03-26 13:16:19 +0900

    feat #52 - SharedRoomPostController 작성
    게시판에서 게시글 불러오는 api 기능 구현

--- src/main/java/org/capstone/maru/controller/AuthController.java
+++ src/main/java/org/capstone/maru/controller/AuthController.java
@@ -2,21 +2,26 @@ package org.capstone.maru.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.capstone.maru.security.principal.MemberPrincipal;
 import org.capstone.maru.security.token.RefreshTokenService;
 import org.capstone.maru.security.token.TokenDto;
 import org.capstone.maru.security.token.TokenReIssuer;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping
 public class AuthController {
-    
+
     private final TokenReIssuer tokenReIssuer;
 
     @PostMapping("/auth/token/refresh")
--- src/main/java/org/capstone/maru/controller/SharedRoomPostController.java
+++ src/main/java/org/capstone/maru/controller/SharedRoomPostController.java
@@ -4,10 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.capstone.maru.annotation.RequestQueryString;
 import org.capstone.maru.dto.request.SearchFilterDto;
+import org.capstone.maru.dto.response.StudioRoomPostResponse;
 import org.capstone.maru.security.principal.MemberPrincipal;
+import org.capstone.maru.service.SharedRoomPostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -16,16 +23,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/shared/posts")
 public class SharedRoomPostController {
 
-    @GetMapping
-    public String sharedRoomPosts(
-        @AuthenticationPrincipal MemberPrincipal principal,
-        @RequestQueryString(name = "filter", required = false) SearchFilterDto searchFilterDto
-    ) {
-        if (searchFilterDto != null) {
-            log.info("[Controller Debug] rentalTypes: {} | roomTypes: {}",
-                searchFilterDto.rentalTypes(), searchFilterDto.roomTypes());
-        }
+    private final SharedRoomPostService sharedRoomPostService;
 
-        return "ok";
+    @GetMapping("/studio")
+    public Page<StudioRoomPostResponse> studioRoomPosts(
+        @AuthenticationPrincipal MemberPrincipal principal,
+        @RequestQueryString(name = "filter", required = false) SearchFilterDto searchFilterDto,
+        @RequestParam(name = "search", required = false) String searchKeyWords,
+        @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC) Pageable pageable
+    ) {
+        return sharedRoomPostService
+            .searchStudioRoomPosts(
+                principal.gender(),
+                searchFilterDto,
+                searchKeyWords,
+                pageable
+            )
+            .map(StudioRoomPostResponse::from);
     }
 }
