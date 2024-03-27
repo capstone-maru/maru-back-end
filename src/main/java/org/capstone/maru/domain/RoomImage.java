0002f05cbf27746c11b328acad1efd0dfbcff481
leejh7 <777joonho@kookmin.ac.kr>
2024-03-27 14:44:26 +0900

    feat #54 - 방 이미지 엔티티, DTO 작성
    아직 이미지를 백엔드에서 어떻게 처리할지 결정하지 않아 기초적인 값만 포함하였음

--- src/main/java/org/capstone/maru/domain/RoomImage.java
+++ src/main/java/org/capstone/maru/domain/RoomImage.java
@@ -8,10 +8,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.util.Objects;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = {"studioRoomPost"})
 @Entity
 public class RoomImage extends AuditingFields {
 
@@ -25,7 +30,49 @@ public class RoomImage extends AuditingFields {
     @Column
     private String storeImagePath;
 
+    @Column
+    private Boolean isThumbnail;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "studio_room_post_id")
     private StudioRoomPost studioRoomPost;
+
+    // -- 생성자 메서드 -- //
+    private RoomImage(Long id, String fileName, String storeImagePath,
+        Boolean isThumbnail, StudioRoomPost studioRoomPost) {
+        this.id = id;
+        this.fileName = fileName;
+        this.storeImagePath = storeImagePath;
+        this.isThumbnail = isThumbnail;
+        this.studioRoomPost = studioRoomPost;
+    }
+
+    public static RoomImage of(
+        Long id,
+        String fileName,
+        String storeImagePath,
+        Boolean isThumbnail,
+        StudioRoomPost studioRoomPost
+    ) {
+        return new RoomImage(
+            id, fileName, storeImagePath, isThumbnail, studioRoomPost
+        );
+    }
+
+    // -- Equals & Hash -- //
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof RoomImage roomImage)) {
+            return false;
+        }
+        return id != null && id.equals(roomImage.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
