ca48244af171ef06fdca4b9a7559a581ab7a31c4
leejh7 <777joonho@kookmin.ac.kr>
2024-03-29 01:40:17 +0900

    feat #54 - 방 있는 메이트 CD 기능 구현

--- src/main/java/org/capstone/maru/service/SharedRoomPostService.java
+++ src/main/java/org/capstone/maru/service/SharedRoomPostService.java
@@ -1,12 +1,17 @@
 package org.capstone.maru.service;
 
 import java.util.Optional;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.capstone.maru.domain.MemberAccount;
 import org.capstone.maru.domain.StudioRoomPost;
+import org.capstone.maru.dto.RoomImageDto;
+import org.capstone.maru.dto.RoomInfoDto;
 import org.capstone.maru.dto.StudioRoomPostDetailDto;
 import org.capstone.maru.dto.StudioRoomPostDto;
 import org.capstone.maru.dto.request.SearchFilterRequest;
+import org.capstone.maru.repository.MemberAccountRepository;
 import org.capstone.maru.repository.StudioRoomPostRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,6 +26,7 @@ import org.springframework.util.StringUtils;
 public class SharedRoomPostService {
 
     private final StudioRoomPostRepository studioRoomPostRepository;
+    private final MemberAccountRepository memberAccountRepository;
 
     @Transactional(readOnly = true)
     public Page<StudioRoomPostDto> searchStudioRoomPosts(
@@ -58,5 +64,29 @@ public class SharedRoomPostService {
             .filter(studioRoomPost -> studioRoomPost.getPublisherGender().equals(gender))
             .map(StudioRoomPostDetailDto::from)
             .orElseThrow(() -> new IllegalArgumentException("그런 게시물은 존재하지 않습니다."));
+    }
+
+    // TODO: 방 이미지 저장 기능 구현하기
+    public void saveStudioRoomPost(
+        String publisherMemberId,
+        StudioRoomPostDto studioRoomPostDto,
+        Set<RoomImageDto> roomImagesDto,
+        RoomInfoDto roomInfoDto
+    ) {
+        MemberAccount publisherAccount = memberAccountRepository.getReferenceById(
+            publisherMemberId);
+
+        studioRoomPostRepository.save(
+            studioRoomPostDto.toEntity(
+                publisherAccount, roomInfoDto.toEntity()
+            )
+        );
+    }
+
+    public void deleteStudioRoomPost(
+        Long postId,
+        String memberId
+    ) {
+        studioRoomPostRepository.deleteByIdAndAndPublisherAccount_MemberId(postId, memberId);
     }
 }
