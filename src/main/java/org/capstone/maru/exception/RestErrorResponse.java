0a749217f915ea03ad421b628e578f5806a6f1d8
leejh7 <777joonho@kookmin.ac.kr>
2024-03-31 07:31:01 +0900

    feat #67 - @valid의 예외 처리

--- src/main/java/org/capstone/maru/exception/RestErrorResponse.java
+++ src/main/java/org/capstone/maru/exception/RestErrorResponse.java
@@ -1,6 +1,8 @@
 package org.capstone.maru.exception;
 
+import java.util.List;
 import lombok.Builder;
+import org.springframework.http.HttpStatus;
 
 /**
  * @param status   에러 상태 코드
@@ -43,6 +45,26 @@ public record RestErrorResponse(
                                 .code(code.getCode())
                                 .errorMsg(code.getMessage())
                                 .reason(reason)
+                                .build();
+    }
+
+    public static RestErrorResponse of(final HttpStatus httpStatus,
+        final String errorMsg) {
+        return RestErrorResponse.builder()
+                                .status(httpStatus.value())
+                                .code(null)
+                                .errorMsg(errorMsg)
+                                .reason(null)
+                                .build();
+    }
+
+    public static RestErrorResponse of(final HttpStatus httpStatus,
+        final List<String> errorMessages) {
+        return RestErrorResponse.builder()
+                                .status(httpStatus.value())
+                                .code(null)
+                                .errorMsg(String.join("\n", errorMessages))
+                                .reason(null)
                                 .build();
     }
 
