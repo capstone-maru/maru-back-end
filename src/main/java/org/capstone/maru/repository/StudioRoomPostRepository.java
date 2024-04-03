ed8a826b1807562b353c454160df6a725e51aeb8
leejh7 <777joonho@kookmin.ac.kr>
2024-04-03 14:09:24 +0900

    feat #69 - fetch join을 사용하여 roomImage 정보 가져오도록 쿼리문 작성
    프록시 문제를 해결하기 위해 변경하였고 노션 이슈 페이지에 자세히 작성할 예정
    fetch join을 사용하지 않고 해결할 수 있는 방법 찾는 중

--- src/main/java/org/capstone/maru/repository/StudioRoomPostRepository.java
+++ src/main/java/org/capstone/maru/repository/StudioRoomPostRepository.java
@@ -5,6 +5,7 @@ import org.capstone.maru.repository.querydsl.StudioRoomPostCustomRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,6 +13,7 @@ public interface StudioRoomPostRepository extends
     JpaRepository<StudioRoomPost, Long>,
     StudioRoomPostCustomRepository {
 
+    @Query("SELECT sp FROM StudioRoomPost sp JOIN FETCH sp.roomImages")
     Page<StudioRoomPost> findAllByPublisherGender(String gender, Pageable pageable);
 
     void deleteByIdAndAndPublisherAccount_MemberId(Long postId, String memberId);
