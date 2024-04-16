4b8bb1e75bf4edf20b0541a7dcf06044189597a1
yeonsu <jack0810@kookmin.ac.kr>
2024-04-16 23:28:37 +0900

    feat #104 - add chat handler socket 연결시의 tocken 인증을 위한 handler 추가

--- src/main/java/org/capstone/maru/controller/ChatController.java
+++ src/main/java/org/capstone/maru/controller/ChatController.java
@@ -8,6 +8,7 @@ import org.capstone.maru.dto.request.ChatMessageRequest;
 import org.capstone.maru.service.ChatService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,10 +29,11 @@ public class ChatController {
     @MessageMapping("/{roomId}") //여기로 전송되면 메서드 호출 -> WebSocketConfig prefixes 에서 적용한건 앞에 생략
     @SendTo("/room/{roomId}")
     //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
-    public ChatMessageRequest chat(@DestinationVariable Long roomId, ChatMessageRequest message) {
+    public ChatMessageRequest chat(@DestinationVariable Long roomId,
+        @Payload ChatMessageRequest message) {
 
         log.info("roomId : {}, message room id : {}", roomId, message.roomId());
-        
+
         if (!roomId.equals(message.roomId())) {
             throw new IllegalArgumentException("roomId is not matched");
         }
