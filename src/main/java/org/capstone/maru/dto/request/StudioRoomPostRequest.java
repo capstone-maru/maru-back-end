23358ffdebe7dc9d31a7df3625cbc2ce20327cf7
leejh7 <777joonho@kookmin.ac.kr>
2024-04-16 13:26:22 +0900

    feat #105 - Spring의 MessageConverter 사용하도록 구조 변경
    게시글 작성 시 게시글 메이트 카드 정보를 받도록 구현

--- src/main/java/org/capstone/maru/dto/request/StudioRoomPostRequest.java
+++ src/main/java/org/capstone/maru/dto/request/StudioRoomPostRequest.java
@@ -1,73 +1,55 @@
 package org.capstone.maru.dto.request;
 
-import com.google.gson.Gson;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.util.Arrays;
 import java.util.List;
 import org.capstone.maru.annotation.ImageFilesConstraints;
 import org.capstone.maru.domain.Address;
 import org.capstone.maru.domain.Address.CITY;
 import org.capstone.maru.domain.constant.RentalType;
 import org.capstone.maru.domain.constant.RoomType;
+import org.capstone.maru.dto.MemberCardDto;
 import org.capstone.maru.dto.RoomImageDto;
 import org.capstone.maru.dto.RoomInfoDto;
 import org.capstone.maru.dto.StudioRoomPostDto;
 
 public record StudioRoomPostRequest(
-    String imageFilesData,
-    @NotBlank(message = "게시글 정보는 반드시 작성해야 합니다.")
-    String postData,
-    @NotBlank(message = "거래 정보는 반드시 작성해야 합니다.")
-    String transactionData,
-    @NotBlank(message = "방 상세 정보는 반드시 작성해야 합니다.")
-    String roomDetailData,
-    @NotBlank(message = "방 위치 정보는 반드시 작성해야 합니다.")
-    String locationData,
+    @ImageFilesConstraints
+    List<@Valid ImageFileData> imageFilesData,
     @Valid
-    ImageFilesData imageFilesInfoData,
+    PostData postData,
     @Valid
-    PostData postInfoData,
+    TransactionData transactionData,
     @Valid
-    TransactionData transactionInfoData,
+    RoomDetailData roomDetailData,
     @Valid
-    RoomDetailData roomInfoData,
-    @Valid
-    LocationData locationInfoData
+    LocationData locationData,
+    MemberFeatureRequest roomMateCardData
 ) {
 
     // -- 생성자 -- //
-    public StudioRoomPostRequest(String imageFilesData, String postData, String transactionData,
-        String roomDetailData, String locationData, ImageFilesData imageFilesInfoData,
-        PostData postInfoData, TransactionData transactionInfoData, RoomDetailData roomInfoData,
-        LocationData locationInfoData) {
-        this.imageFilesData = imageFilesData;
-        this.postData = postData;
-        this.transactionData = transactionData;
-        this.roomDetailData = roomDetailData;
-        this.locationData = locationData;
-
-        this.imageFilesInfoData = ImageFilesData.fromJson(imageFilesData);
-        this.postInfoData = PostData.fromJson(postData);
-        this.transactionInfoData = TransactionData.fromJson(transactionData);
-        this.roomInfoData = RoomDetailData.fromJson(roomDetailData);
-        this.locationInfoData = LocationData.fromJson(locationData);
-    }
-
     public StudioRoomPostDto toBaseStudioRoomPostDto(String publisherGender) {
         return StudioRoomPostDto
             .builder()
-            .title(postInfoData.title)
-            .content(postInfoData.content)
+            .title(postData.title)
+            .content(postData.content)
             .publisherGender(publisherGender)
             .build();
     }
 
+    public MemberCardDto toMemberCardDto() {
+        return MemberCardDto
+            .builder()
+            .location(roomMateCardData.location())
+            .myFeatures(roomMateCardData.features())
+            .build();
+    }
+
     public List<RoomImageDto> toRoomImagesDto() {
-        return imageFilesInfoData.imageFiles
+        return imageFilesData
             .stream()
             .map(imageFileData ->
                 RoomImageDto
@@ -84,49 +66,37 @@ public record StudioRoomPostRequest(
         return RoomInfoDto
             .builder()
             .address(
-                Address.of(locationInfoData.city, locationInfoData.oldAddress,
-                    locationInfoData.roadAddress, locationInfoData.detailAddress,
-                    locationInfoData.stationName,
-                    locationInfoData.stationTime, locationInfoData.busStopTime,
-                    locationInfoData.schoolName, locationInfoData.schoolTime,
-                    locationInfoData.convenienceStoreTime
+                Address.of(locationData.city, locationData.oldAddress,
+                    locationData.roadAddress, locationData.detailAddress,
+                    locationData.stationName,
+                    locationData.stationTime, locationData.busStopTime,
+                    locationData.schoolName, locationData.schoolTime,
+                    locationData.convenienceStoreTime
                 )
             )
-            .roomType(roomInfoData.roomType)
-            .size(roomInfoData.size)
-            .numberOfRoom(roomInfoData.numberOfRoom)
-            .rentalType(transactionInfoData.rentalType)
-            .price(transactionInfoData.price)
-            .monthlyFee(transactionInfoData.monthlyFee)
-            .managementFee(transactionInfoData.managementFee)
-            .expectedPayment(transactionInfoData.expectedPayment)
-            .recruitmentCapacity(roomInfoData.recruitmentCapacity)
+            .roomType(roomDetailData.roomType)
+            .size(roomDetailData.size)
+            .numberOfRoom(roomDetailData.numberOfRoom)
+            .rentalType(transactionData.rentalType)
+            .price(transactionData.price)
+            .monthlyFee(transactionData.monthlyFee)
+            .managementFee(transactionData.managementFee)
+            .expectedPayment(transactionData.expectedPayment)
+            .recruitmentCapacity(roomDetailData.recruitmentCapacity)
             .build();
     }
 
     // -- Nested -- //
-    public record ImageFilesData(
-        @ImageFilesConstraints
-        List<@Valid ImageFileData> imageFiles
+    public record ImageFileData(
+        @NotBlank(message = "업로드 된 이미지 파일 이름을 작성해야 합니다.")
+        String fileName,
+        @NotNull(message = "썸네일인지 아닌지 boolean 값으로 작성해야 합니다.")
+        Boolean isThumbNail,
+        @Min(value = 1, message = "순서는 1부터 시작합니다.")
+        @Max(value = 10, message = "11개 이상의 순서는 있을 수 없습니다.")
+        Short order
     ) {
 
-        public static ImageFilesData fromJson(String json) {
-            Gson gson = new Gson();
-            List<ImageFileData> result = Arrays.asList(gson.fromJson(json, ImageFileData[].class));
-            return new ImageFilesData(result);
-        }
-
-        public record ImageFileData(
-            @NotBlank(message = "업로드 된 이미지 파일 이름을 작성해야 합니다.")
-            String fileName,
-            @NotNull(message = "썸네일인지 아닌지 boolean 값으로 작성해야 합니다.")
-            Boolean isThumbNail,
-            @Min(value = 1, message = "순서는 1부터 시작합니다.")
-            @Max(value = 10, message = "11개 이상의 순서는 있을 수 없습니다.")
-            Short order
-        ) {
-
-        }
     }
 
     public record PostData(
@@ -136,10 +106,6 @@ public record StudioRoomPostRequest(
         String content
     ) {
 
-        private static PostData fromJson(String json) {
-            Gson gson = new Gson();
-            return gson.fromJson(json, PostData.class);
-        }
     }
 
     public record TransactionData(
@@ -159,11 +125,6 @@ public record StudioRoomPostRequest(
         Long expectedPayment
     ) {
 
-        private static TransactionData fromJson(String json) {
-            Gson gson = new Gson();
-            return gson.fromJson(json, TransactionData.class);
-        }
-
     }
 
     public record RoomDetailData(
@@ -179,11 +140,6 @@ public record StudioRoomPostRequest(
         @Max(value = 10, message = "모집 인원에 이상치 값이 입력 되었습니다.")
         Short recruitmentCapacity
     ) {
-
-        private static RoomDetailData fromJson(String json) {
-            Gson gson = new Gson();
-            return gson.fromJson(json, RoomDetailData.class);
-        }
 
     }
 
@@ -204,11 +160,6 @@ public record StudioRoomPostRequest(
         @Min(value = 0, message = "시간은 음수 일 수 없습니다.")
         Short convenienceStoreTime
     ) {
-
-        private static LocationData fromJson(String json) {
-            Gson gson = new Gson();
-            return gson.fromJson(json, LocationData.class);
-        }
 
     }
 
