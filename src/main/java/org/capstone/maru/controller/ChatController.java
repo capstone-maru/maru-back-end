81847dc81d2a75f5b871a424b5143b60e41d40ae
yeonsu <jack0810@kookmin.ac.kr>
2024-04-24 02:41:40 +0900

    fix #114 - fix api reponse type reponse type을 api response로 변경

--- src/main/java/org/capstone/maru/controller/ChatController.java
+++ src/main/java/org/capstone/maru/controller/ChatController.java
@@ -6,9 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.capstone.maru.dto.request.ChatPageRequest;
 import org.capstone.maru.dto.request.ChatRoomMemberRequest;
 import org.capstone.maru.dto.request.ChatRoomRequest;
+import org.capstone.maru.dto.response.APIResponse;
 import org.capstone.maru.dto.response.ChatMessageResponse;
 import org.capstone.maru.security.principal.MemberPrincipal;
 import org.capstone.maru.service.ChatService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,59 +31,66 @@ public class ChatController {
     채팅방 생성
      */
     @PostMapping
-    public Long chatRoom(
+    public ResponseEntity<APIResponse> chatRoom(
         @AuthenticationPrincipal MemberPrincipal memberPrincipal,
         @RequestBody ChatRoomRequest roomRequest
     ) {
         log.info("memberPrincipal : {}", memberPrincipal.memberId());
 
-        return chatService.createChatRoom(memberPrincipal.memberId(), roomRequest.roomName(),
+        Long data = chatService.createChatRoom(memberPrincipal.memberId(), roomRequest.roomName(),
             roomRequest.members()).getId();
+
+        return ResponseEntity.ok(APIResponse.success(data));
     }
 
     /*
     채팅방 멤버 보여주기
      */
     @GetMapping("/{roomId}")
-    public List<String> showChatRoomMember(@PathVariable Long roomId) {
-        return chatService.showChatRoomMember(roomId);
+    public ResponseEntity<APIResponse> showChatRoomMember(@PathVariable Long roomId) {
+        List<String> data = chatService.showChatRoomMember(roomId);
+        return ResponseEntity.ok(APIResponse.success(data));
     }
 
     /*
     채팅방 보여주기
      */
     @GetMapping
-    public List<String> showChatRoom(@AuthenticationPrincipal MemberPrincipal memberPrincipal) {
+    public ResponseEntity<APIResponse> showChatRoom(
+        @AuthenticationPrincipal MemberPrincipal memberPrincipal) {
 
         log.info("memberPrincipal : {}", memberPrincipal.memberId());
 
         List<String> data = chatService.showChatRoom(memberPrincipal.memberId());
 
         log.info("data : {}", data);
-        return data;
+        return ResponseEntity.ok(APIResponse.success(data));
     }
 
     /*
     채팅방에 멤버 추가하기
      */
     @PostMapping("/{roomId}/member")
-    public void addChatRoomMember(@PathVariable Long roomId,
+    public ResponseEntity<APIResponse> addChatRoomMember(@PathVariable Long roomId,
         @RequestBody ChatRoomMemberRequest memberId) {
         log.info("roomId : {}, memberId : {}", roomId, memberId);
         chatService.addChatRoomMember(roomId, memberId.members());
+
+        return ResponseEntity.ok(APIResponse.success("success"));
     }
+
 
     /*
     최근 채팅 조회
      */
     @PostMapping("/chat")
-    public List<ChatMessageResponse> showChat(@RequestBody ChatPageRequest chatPageRequest) {
+    public ResponseEntity<APIResponse> showChat(@RequestBody ChatPageRequest chatPageRequest) {
         log.info("roomId : {}", chatPageRequest.roomId());
         List<ChatMessageResponse> data = chatService.getChatMessages(chatPageRequest.roomId(),
             chatPageRequest.size(), chatPageRequest.page());
 
         log.info("{}", data);
-        return data;
+        return ResponseEntity.ok(APIResponse.success(data));
     }
 }
 
