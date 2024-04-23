d16266466645eeb70f2d3b13aae04cdc503f8d3d
yeonsu <jack0810@kookmin.ac.kr>
2024-04-24 02:34:02 +0900

    feat #114 - 페이징 쿼리
    페이징을 위한 함수 추가

--- src/main/java/org/capstone/maru/repository/mongodb/ChatRepository.java
+++ src/main/java/org/capstone/maru/repository/mongodb/ChatRepository.java
@@ -1,8 +1,11 @@
 package org.capstone.maru.repository.mongodb;
 
 import org.capstone.maru.domain.Chat;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ChatRepository extends MongoRepository<Chat, Long> {
+public interface ChatRepository extends MongoRepository<Chat, String> {
 
+    Page<Chat> findAllByRoomId(Long roomId, Pageable pageable);
 }
