d9946e4141de3c3a7f4b7d87de409a23470698ed
yeonsu <jack0810@kookmin.ac.kr>
2024-04-24 15:09:32 +0900

    feat #114 - 채팅방 간단 프로필 생성
    채팅방 간단 프로필 생성 로직

--- src/main/java/org/capstone/maru/dto/response/ChatMemberProfileResponse.java
+++ src/main/java/org/capstone/maru/dto/response/ChatMemberProfileResponse.java
@@ -1,2 +1,21 @@
-package org.capstone.maru.dto.response;public class ChatMemberProfileResponse {
+package org.capstone.maru.dto.response;
+
+import lombok.Builder;
+
+@Builder
+public record ChatMemberProfileResponse(
+    String memberId,
+    String nickname,
+    String profileImageUrl
+) {
+
+    public static ChatMemberProfileResponse from(String memberId, String nickname,
+        String profileImageUrl) {
+        return ChatMemberProfileResponse
+            .builder()
+            .memberId(memberId)
+            .nickname(nickname)
+            .profileImageUrl(profileImageUrl)
+            .build();
+    }
 }
