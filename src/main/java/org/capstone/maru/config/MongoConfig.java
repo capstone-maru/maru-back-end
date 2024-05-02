9d23645db973b0ad40be9f54ade8a56b4edab7ab
yeonsu <jack0810@kookmin.ac.kr>
2024-05-02 13:34:03 +0900

    fix cicd file

--- src/main/java/org/capstone/maru/config/MongoConfig.java
+++ src/main/java/org/capstone/maru/config/MongoConfig.java
@@ -1,11 +1,7 @@
 package org.capstone.maru.config;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
