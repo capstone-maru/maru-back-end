571fa0056391a22c70216830b0083143f4bb5128
leejh7 <777joonho@kookmin.ac.kr>
2024-05-07 19:35:24 +0900

    feat #116 - 기숙사 게시글 엔티티 작성

--- src/main/java/org/capstone/maru/domain/DormitoryRoomPost.java
+++ src/main/java/org/capstone/maru/domain/DormitoryRoomPost.java
@@ -1,2 +1,42 @@
-package org.capstone.maru.domain;public class DormitoryRoomPost {
+package org.capstone.maru.domain;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("D")
+@DynamicUpdate
+@Entity
+public class DormitoryRoomPost extends SharedRoomPost {
+
+    // -- 생성자 메서드 -- //
+
+    private DormitoryRoomPost(String title, String content, String publisherGender,
+        MemberAccount publisherAccount, FeatureCard roomMateCard) {
+        super(title, content, publisherGender, publisherAccount, roomMateCard);
+    }
+
+    public static DormitoryRoomPost of(
+        String title,
+        String content,
+        String publisherGender,
+        MemberAccount publisherAccount,
+        FeatureCard roomMateCard
+    ) {
+        return new DormitoryRoomPost(
+            title, content, publisherGender, publisherAccount, roomMateCard
+        );
+    }
+
+    // -- 비지니스 로직 -- //
+    public void updateDormitoryRoomPost(
+
+    ) {
+        
+    }
 }
