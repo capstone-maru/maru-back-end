c5ee0df6b6535e8037cbcfb075c74c0317b7ef77
leejh7 <777joonho@kookmin.ac.kr>
2024-05-29 16:33:44 +0900

    feat #193 - 채팅방 이름 변경 / 채팅방 완전히 나가기 기능 구현

--- src/main/java/org/capstone/maru/repository/postgre/MemberRoomRepository.java
+++ src/main/java/org/capstone/maru/repository/postgre/MemberRoomRepository.java
@@ -1,9 +1,14 @@
 package org.capstone.maru.repository.postgre;
 
+import java.util.Optional;
 import org.capstone.maru.domain.MemberRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRoomRepository extends JpaRepository<MemberRoom, Long> {
 
-    MemberRoom findByMemberIdAndChatRoomId(String memberId, Long chatRoomId);
+    Optional<MemberRoom> findByMemberIdAndChatRoomId(String memberId, Long chatRoomId);
+
+    void deleteByMemberIdAndChatRoomId(String memberId, Long chatRoomId);
+
+    boolean existsByChatRoomId(Long chatRoomId);
 }
